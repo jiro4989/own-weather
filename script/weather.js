07ea179bc5b40925704e2f130340c6927f2a6c24
jiro4989 <jiroron666@gmail.com>
2019-07-07 22:42:07 +0900

    :sparkles: ユーザ名をスプレッドシートの値から決定できるようにした

--- script/weather.js
+++ script/weather.js
@@ -1,40 +1,57 @@
 // weatherUserName はTwitterのユーザー名に指定の天気と時刻を付与します。
 function weatherUserName() {
+  var weather = fetchWeather();
+  var weatherIcon = toWeatherIcon(weather);
+  
+  // DB用途のスプレッドシートからセットするユーザ名に使用するテキストを取得
+  var dbSheetId = PropertiesService.getScriptProperties().getProperty("DB_SHEET_ID");
+  var dbSheet = SpreadsheetApp.openById(dbSheetId);
+  var userName = dbSheet.getRange('B1').getValue();
+  postUpdateUserName(userName + weatherIcon + "[" + formatTime(new Date()) + "]");
+  
+  // 名前が変化しているときだけつぶやきで通知する
+  var befName = dbSheet.getRange('B2').getValue();
+  if (befName !== userName) {
+    var userId = PropertiesService.getScriptProperties().getProperty("TWITTER_USER_ID");
+    postTweet('@' + userId + '\n# usermod -l ' + userName + ' ' + befName + '\n# usermod -m -d /home/' + userName + ' ' + userName);
+  }
+}
+
+function fetchWeather() {
   var city = PropertiesService.getScriptProperties().getProperty("CITY");
   var country = PropertiesService.getScriptProperties().getProperty("COUNTRY");
   var apiKey = PropertiesService.getScriptProperties().getProperty("OPEN_WEATHER_MAP_API_KEY");
   var url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "," + country + "&appid=" + apiKey;
-  var json = UrlFetchApp.fetch(url).getContentText();
+  return UrlFetchApp.fetch(url).getContentText();
+}
+
+// 天気データを天気アイコンにする
+function toWeatherIcon(json) {
   var jsonData = JSON.parse(json);
   var weatherCode = jsonData.weather[0].id;
   var weather = Math.floor(weatherCode / 100);
-  
-  var weatherIconString = "";
+  var weatherIcon = getWeatherIcon(weather);
+  Logger.log(weatherIcon);
+  return weatherIcon;
+}
+
+// 都市の天気データを取得する
+function getWeatherIcon(weather) {
   switch (weather) {
     case 2:
     case 3:
     case 5:
-      weatherIconString = "☔️";
-      break;
+      return "☔️";
     case 6:
-      weatherIconString = "☃️";
-      break;
+      return "☃️";
     case 7:
-      weatherIconString = "🌫️";
-      break;
+      return "🌫️";
     case 8:
-      if (weatherCode == 800) weatherIconString = "🌞";
-      else weatherIconString = "☁️";
-      break;
+      if (weatherCode == 800) return "🌞";
+      else return "☁️";
     default:
-      weatherIconString = "❓";
-      break;
+      return "❓";
   }
-  Logger.log(weatherIconString);
-  
-  var userName = PropertiesService.getScriptProperties().getProperty("TWITTER_USER_NAME");
-  userName = userName + weatherIconString + "[" + formatTime(new Date()) + "]";
-  postUpdateUserName(userName);
 }
 
 // formatTime はDate変数から時刻文字列を生成します。
